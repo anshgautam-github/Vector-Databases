bf35711e75d837e2f8c3c1fde7ed15a4bf9c825c
Ansh Gautam <anshgautam1011@gmail.com>
2026-07-11 12:21:57 +0530

    code added

---
+++ Similarity Search with FAISS/01-Flat Index.py
@@ -0,0 +1,25 @@
+import faiss
+import numpy as np
+
+
+# Step 1: Create sample vectors
+d = 128                     # Vector dimension
+nb = 10000                  # Number of database vectors
+nq = 5                      # Number of queries
+
+xb = np.random.random((nb, d)).astype("float32")
+xq = np.random.random((nq, d)).astype("float32")
+
+
+# Step 2: Create Flat Index
+index = faiss.IndexFlatL2(d)
+
+# Step 3: Add vectors
+index.add(xb)
+
+# Step 4: Search
+k = 5
+D, I = index.search(xq, k)
+
+print(I)
+print(D)
